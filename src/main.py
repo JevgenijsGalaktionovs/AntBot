3a579b09c774591a09a8c98cbb8e7653690acc5b
Eugene <jgalak16@student.aau.dk>
2019-03-08 12:41:39 +0100

    Big Kinematics update

--- src/main.py
+++ src/main.py
@@ -1,32 +1,63 @@
 #!/usr/bin/env python
 
 import rospy
-from   control_interface import AntBot
-from   dynamixel_library import *
 import time
-from kinematics import *
+
+from kinematics        import *
+from dynamixel_library import *
+from stupid_walk       import *
 
 
- 	
+def Gait1(my_list):
+    Write1Pos(1,my_list[0])
+    Write1Pos(2,my_list[1])
+    Write1Pos(3,my_list[2])
+    Write1Pos(10,my_list[9])
+    Write1Pos(11,my_list[10])
+    Write1Pos(12,my_list[11])
+    Write1Pos(13,my_list[12])
+    Write1Pos(14,my_list[13])
+    Write1Pos(15,my_list[14])
+
+def Gait2(my_list):
+    Write1Pos(4,my_list[3])
+    Write1Pos(5,my_list[4])
+    Write1Pos(6,my_list[5])
+    Write1Pos(7,my_list[6])
+    Write1Pos(8,my_list[7])
+    Write1Pos(9,my_list[8])
+    Write1Pos(16,my_list[15])
+    Write1Pos(17,my_list[16])
+    Write1Pos(18,my_list[17])
 
 
 if __name__=='__main__':
     try:
-		DisableTorqueAllServos()
-		time.sleep(3)
-		EnableTorqueAllServos()
-		alternative_stand_up()
-		time.sleep(2)
+        DisableTorqueAllServos()
+        time.sleep(3)
+        EnableTorqueAllServos()
+        stand_up()
+        time.sleep(2)
 		# PrintForward()
 		#WALK()
-		y = int(input("How much do you want to move forward in mm pr step? "))
-		z=50#hight
-		x = int(input("How much do you want to move sideways in mm pr step? "))
-		while(1):
-			Inverse_kinemat_gait1(x/2,y/2,z)
-			Inverse_kinemat_gait1(x,y,-z)
-			Inverse_kinemat_gait2(x/2,y/2,z)
-			Inverse_kinemat_gait2(x,y,-z)
-			stand_up()
+        y = int(input("How much do you want to move forward in mm pr step? "))
+        z=50#height
+        x = int(input("How much do you want to move sideways in mm pr step? "))
+        while(1):
+
+            my_list = Inverse_kinematics(x/2,y/2,z)
+            ae = [int(x) for x in my_list]
+            Gait1(ae)
+            my_list = Inverse_kinematics(x,y,-z)
+            ae = [int(x) for x in my_list]
+            Gait1(ae)
+
+            my_list = Inverse_kinematics(x/2,y/2,z)
+            ae = [int(x) for x in my_list]
+            Gait2(ae)
+            my_list = Inverse_kinematics(x,y,-z)
+            ae = [int(x) for x in my_list]
+            Gait2(ae)
+            stand_up()
     except rospy.ROSInterruptException :
         portHandler.closePort()
--- src/stupid_walk.py
+++ src/stupid_walk.py
@@ -3,7 +3,7 @@ import time
 
 
 
-def alternative_stand_up():
+def stand_up():
 
     # First Tripod
     #Leg 1
@@ -39,35 +39,9 @@ def alternative_stand_up():
 	Write1Pos(11,1878)
 	Write1Pos(12,3048)
 	time.sleep(1)
-def stand_up():
 
-    # First Tripod
-    #Leg 1
-	Write1Pos(1,2048)
-	Write1Pos(2,2218)
-	Write1Pos(3,1024)
-    # #Leg 4
-	# Write1Pos(10,2048)
-	# Write1Pos(11,1878)
-	# Write1Pos(12,3048)
-    #Leg 5
-	Write1Pos(13,2048)
-	Write1Pos(14,2218)
-	Write1Pos(15,1024)
-
-    # Second Tripod
-    #Leg 2
-	Write1Pos(4,2048)
-	Write1Pos(5,1878)
-	Write1Pos(6,3048)
-    # #Leg 3
-	# Write1Pos(7,2048)
-	# Write1Pos(8,2218)
-	# Write1Pos(9,1024)
-    #Leg 6
-	Write1Pos(16,2048)
-	Write1Pos(17,1878)
-	Write1Pos(18,3048)
+# Iteration 1 : Australopithecus
+############################
 def step1():
     # First Tripod
     #Leg 1
@@ -297,7 +271,7 @@ def step8():
 	Write1Pos(16,2048)
 	Write1Pos(17,1878)
 	Write1Pos(18,3048)
-def WALK():
+def WALK1():
 	stand_up()
 	time.sleep(0.1)
 	step1()
@@ -318,3 +292,86 @@ def WALK():
 		time.sleep(0.1)
 		step8()
 		time.sleep(0.1)
+
+def Inverse_kinemat_gait1(x,y,z):
+    endtip_xyz = Forward_kinematics()
+    servo_array = []
+    servo_array.extend(CalculateInverseLeg1(x,y,z,endtip_xyz))
+    #servo_array.extend(CalculateInverseLeg2(x,y,z,endtip_xyz))
+    #servo_array.extend(CalculateInverseLeg3(x,y,z,endtip_xyz))
+    servo_array.extend(CalculateInverseLeg4(x,y,z,endtip_xyz))
+    servo_array.extend(CalculateInverseLeg5(x,y,z,endtip_xyz))
+    #servo_array.extend(CalculateInverseLeg6(x,y,z,endtip_xyz))
+
+    b=[float(i) for i in servo_array]
+    servo1 = int(b[0])
+    servo2 =int(b[1])
+    servo3 = int(b[2])
+    servo10 = int(b[3])
+    servo11 =int(b[4])
+    servo12 = int(b[5])
+    servo13 = int(b[6])
+    servo14 =int(b[7])
+    servo15 = int(b[8])
+    if servo10 > 4096:
+	servo10 = servo10-4096
+    print(servo10)
+
+    Write1Pos(1,servo1)
+    Write1Pos(2,servo2)
+    Write1Pos(3,servo3)
+    Write1Pos(10,servo10)
+    Write1Pos(11,servo11)
+    Write1Pos(12,servo12)
+    Write1Pos(13,servo13)
+    Write1Pos(14,servo14)
+    Write1Pos(15,servo15)
+
+    return servo_array #OUTPUT: 18 servo thetas to accomplish this motion (rads)
+def Inverse_kinemat_gait2(x,y,z):
+    endtip_xyz = Forward_kinematics()
+    servo_array = []
+    #servo_array.extend(CalculateInverseLeg1(x,y,z,endtip_xyz))
+    servo_array.extend(CalculateInverseLeg2(x,y,z,endtip_xyz))
+    servo_array.extend(CalculateInverseLeg3(x,y,z,endtip_xyz))
+    #servo_array.extend(CalculateInverseLeg4(x,y,z,endtip_xyz))
+    #servo_array.extend(CalculateInverseLeg5(x,y,z,endtip_xyz))
+    servo_array.extend(CalculateInverseLeg6(x,y,z,endtip_xyz))
+    b=[float(i) for i in servo_array]
+    servo4 = int(b[0])
+    servo5 =int(b[1])
+    servo6 = int(b[2])
+    servo7 = int(b[3])
+    servo8 =int(b[4])
+    servo9 = int(b[5])
+    servo16 = int(b[6])
+    servo17 =int(b[7])
+    servo18 = int(b[8])
+
+    Write1Pos(4,servo4)
+    Write1Pos(5,servo5)
+    Write1Pos(6,servo6)
+    Write1Pos(7,servo7)
+    Write1Pos(8,servo8)
+    Write1Pos(9,servo9)
+    Write1Pos(16,servo16)
+    Write1Pos(17,servo17)
+    Write1Pos(18,servo18)
+
+    return servo_array #OUTPUT: 18 servo thetas to accomplish this motion (rads)
+############################
+
+# Iteration 2 : Homo habilis
+############################
+def WALK2():
+	#WALK()
+	y = int(input("How much do you want to move forward in mm pr step? "))
+	z=50#hight
+	x = int(input("How much do you want to move sideways in mm pr step? "))
+	while(1):
+		Inverse_kinemat_gait1(x/2,y/2,z)
+		Inverse_kinemat_gait1(x,y,-z)
+		Inverse_kinemat_gait2(x/2,y/2,z)
+		Inverse_kinemat_gait2(x,y,-z)
+		stand_up()
+############################
