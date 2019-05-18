f68cb16f2646d67879261310f60c2210d9bd26a9
Lars Væhrens <lvahre16@student.aau.dk>
2019-05-18 10:15:22 +0200

    Changed the documentation of the subscriber node.

--- src/sub_tactile.py
+++ src/sub_tactile.py
@@ -4,18 +4,16 @@
 import rospy
 from std_msgs.msg import Int16
 
+    # Function to write the published message from the FSR
+    # to log and terminal when new data is received.
 def callback(msg):
     rospy.loginfo(rospy.get_caller_id() + 'I heard %s', msg.data)
 
 def tac_sub():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
+    # Create a node for the subscriber
     rospy.init_node('sub_tactile', anonymous=True)
 
+    # A subscriber for each of the topics managing each FSR
     rospy.Subscriber('tactile_1', Int16, callback)
     rospy.Subscriber('tactile_2', Int16, callback)
     rospy.Subscriber('tactile_3', Int16, callback)
