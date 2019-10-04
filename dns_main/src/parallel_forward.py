00a0d338c9d4f03ce01bd8cf271ce3ba7067d55b
Rebecca <rmalih16@student.aau.dk>
2019-10-04 10:04:11 +0200

    rotation around z with no condition

--- dns_main/src/parallel_forward.py
+++ dns_main/src/parallel_forward.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python2
 import time
-from math import pi, cos, sin, atan2, acos, sqrt, pow, radians
+from math            import pi, cos, sin, atan2, acos, sqrt, pow, radians
 import numpy as np
-
-from service_router import *
-from kinematics     import Kinematics
-from locomotion     import *
-from service_router import *
+import numpy.matlib
+from service_router  import *
+from kinematics      import Kinematics
+from locomotion      import *
+from service_router  import *
 
 
 ######
@@ -22,10 +22,20 @@ parallelGait(0, pi, -pi, 0, 0, 0)
 time.sleep(2)
 ee_xyz,servopos = np.array( K.doFkine(readPos()))
 p1 =  ee_xyz[0:3]
-p4 = ee_xyz[9:12]#np.around( ee_xyz[9:12])
-p5 = ee_xyz[12:15]#np.around( ee_xyz[12:15])
+p2 =  ee_xyz[3:6]
+p3 =  ee_xyz[6:9]
+p4 =  ee_xyz[9:12]#np.around( ee_xyz[9:12])
+p5 =  ee_xyz[12:15]#np.around( ee_xyz[12:15])
+
 p45 = p4-p5#np.around(p4-p5, decimals = 2)
 p41 = p4-p1#np.around(p4-p1, decimals = 2)
+
+p61 = p6-p1
+length_p61 = sqrt(pow(p61[0],2)+pow(p61[1],2)+pow(p61[2],2))
+p52 = p5-p2
+length_p52 = sqrt(pow(p52[0],2)+pow(p52[1],2)+pow(p52[2],2))
+p43 = p4-p3
+length_p43 = sqrt(pow(p43[0],2)+pow(p43[1],2)+pow(p43[2],2))
 normz = np.cross(p45,p41)
 normz_rounded = np.around(normz)
 print(normz_rounded)
@@ -36,3 +46,7 @@ beta = atan2(normz[0],normz[2])*180/pi
 gamma = -atan2(normz[1],normz[2])*180/pi
 print(beta)
 print(gamma)
+angle_p61_p52 = (180*np.dot(p61,p52))/(length_p61*lemgth_p52*pi)
+angle_p52_p43 = (180*np.dot(p52,p43))/(length_p52*lemgth_p43*pi)
+print(angle_p61_p52)
+print(angle_p52_p43)
