5a00a6c8453f38577545612bc4a96bc9f0cd41a6
Lars Væhrens <lvahre16@student.aau.dk>
2019-10-10 15:58:44 +0200

    New attempts at making position sensor work
    Now getting NAN and 0, but no values.

--- Webots/hexapod/controllers/hexapod_controller/hexapod_controller.py
+++ Webots/hexapod/controllers/hexapod_controller/hexapod_controller.py
@@ -10,24 +10,19 @@ POSITION_SENSOR_SAMPLE_PERIOD = 100
 
 
 class Controller():
-
+    servos = []
+    psNames = []
+    ps = []
+    psValues = []
+    
+    
     def __init__(self):
-
-        # Initialise the Webots Supervisor.
         self.robot = Robot()
         self.timeStep = int(4 * self.robot.getBasicTimeStep())
         self.keyboard = self.robot.getKeyboard()
-
-        # Initialise the leg servos.
-        self.servos = []
         self.init_servos()
-
-        # Initialise the servo position sensors
         self.init_positional_sensors()
-        #self.position_sensors = []
-        self.psNames = []
-        self.ps = []
-        self.psValues = []
+
 
     def init_servos(self):
                           # coxa,     femur,    tibia
@@ -38,8 +33,8 @@ class Controller():
                            'c5_ser', 'f5_ser', 't5_ser',   # LEG 5
                            'c6_ser', 'f6_ser', 't6_ser',): # LEG 6
             servos = self.robot.getMotor(servosName)
-            # servos.setPosition(float('inf'))
-            # servos.setVelocity(0)
+            #servos.setPosition(float('inf'))
+            #servos.setVelocity(0)
             self.servos.append(servos)
 
 #    def init_positional_sensors(self):
@@ -56,15 +51,14 @@ class Controller():
             
             
     def init_positional_sensors(self):
-        ps = []
         psNames = ['c1_pos', 'f1_pos', 't1_pos',   # LEG 1
                    'c2_pos', 'f2_pos', 't2_pos',   # LEG 2
                    'c3_pos', 'f3_pos', 't3_pos',   # LEG 3
                    'c4_pos', 'f4_pos', 't4_pos',   # LEG 4
                    'c5_pos', 'f5_pos', 't5_pos',   # LEG 5
                    'c6_pos', 'f6_pos', 't6_pos',]  # LEG 6
-        for i in range(len(psNames)):
-            self.append(self.robot.getPositionSensor(psNames[i]))
+        for i in range(18):
+            self.ps.append(self.robot.getPositionSensor(psNames[i]))
             self.ps[i].enable(POSITION_SENSOR_SAMPLE_PERIOD)
             
     def run(self):
@@ -76,7 +70,7 @@ class Controller():
             break
                 
     def read(self):
-        for i in range(len(self.psNames)):   
+        for i in range(18):   
             self.psValues.append(self.ps[i].getValue())
         return  self.psValues
             
---
+++ Webots/hexapod/controllers/test/test.py
@@ -0,0 +1,52 @@
+from controller import Robot, Node, Motor, PositionSensor
+
+
+TIME_STEP = 64
+
+robot = Robot()
+timeStep = int(4 * robot.getBasicTimeStep())
+keyboard = robot.getKeyboard()
+
+# Define lists for servo and sensor values
+servos = []
+ps = []
+    
+# Initialise servos and sensors
+             # coxa,     femur,    tibia
+servosName = ['c1_ser', 'f1_ser', 't1_ser',   # LEG 1
+              'c2_ser', 'f2_ser', 't2_ser',   # LEG 2
+              'c3_ser', 'f3_ser', 't3_ser',   # LEG 3
+              'c4_ser', 'f4_ser', 't4_ser',   # LEG 4
+              'c5_ser', 'f5_ser', 't5_ser',   # LEG 5
+              'c6_ser', 'f6_ser', 't6_ser',]  # LEG 6
+for name in servosName:
+    servos.append(robot.getMotor(name))
+            
+         # coxa,     femur,    tibia
+psName = ['c1_pos', 'f1_pos', 't1_pos',   # LEG 1
+          'c2_pos', 'f2_pos', 't2_pos',   # LEG 2
+          'c3_pos', 'f3_pos', 't3_pos',   # LEG 3
+          'c4_pos', 'f4_pos', 't4_pos',   # LEG 4
+          'c5_pos', 'f5_pos', 't5_pos',   # LEG 5
+          'c6_pos', 'f6_pos', 't6_pos',]  # LEG 6
+for i in range(18):
+    ps.append(robot.getPositionSensor(psName[i]))
+    ps[i].enable(TIME_STEP)
+  
+psValues = []
+for i in range(18):
+    psValues.append(ps[i].getValue())
+print(psValues)
+
+# Set positions for each servo to stand
+positions = [0.1, 1.0, 2, 0.1, 1.0, 2, 0.1, 1.0, 2, 0.1, 1.0, 2, 0.1, 1.0, 2, 0.1, 1.0, 2]
+while robot.step(timeStep) != 1:
+    for servo, position in zip(servos, positions):
+        servo.setPosition(position)
+    break
+            
+# Read positions for all servos
+psValues = []
+for i in range(18):
+    psValues.append(ps[i].getValue())
+print(psValues)
