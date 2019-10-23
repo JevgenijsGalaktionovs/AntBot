4a3333e75baf65e7c83294d3607f5f41bc1a5a8d
Rebecca Malihi <rmalih16@student.aau.dk>
2019-10-23 06:04:10 -0700

    creating stairs function
    first

---
+++ dns_main/src/stairs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+from math import pi
+import time
+from service_router import *
+from kinematics     import Kinematics
+from locomotion     import *
+
+
+riser = 100 #mm
+depth = 254 #mm
+tripodGait(5, y, riser+3, 10)
+
+
